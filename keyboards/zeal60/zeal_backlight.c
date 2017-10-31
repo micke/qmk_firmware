a6bbcad77f05af00821dcb7894523fe6fb4316ad
Micke Lisinge <me@mike.gg>
2017-10-31 22:29:53 +0100

    Change colors

--- keyboards/zeal60/zeal_backlight.c
+++ keyboards/zeal60/zeal_backlight.c
@@ -23,14 +23,14 @@ zeal_backlight_config g_config = {
 	.disable_when_usb_suspended = BACKLIGHT_DISABLE_WHEN_USB_SUSPENDED,
 	.disable_after_timeout = BACKLIGHT_DISABLE_AFTER_TIMEOUT,
 	.brightness = 255,
-	.effect = 255, // Default to RGB test, so Zeal can flash and test in one pass!
+	.effect = 2, // Default to RGB test, so Zeal can flash and test in one pass!
 	.effect_speed = 0,
 	.color_1 = { .h = 0, .s = 0, .v = 255 },
 	.color_2 = { .h = 20, .s = 255, .v = 255 },
 	.caps_lock_indicator = { .color = { .h = 0, .s = 0, .v = 255 }, .index = 255 },
 	.layer_1_indicator = { .color = { .h = 165, .s = 255, .v = 255 }, .index = 254 },
 	.layer_2_indicator = { .color = { .h = 20, .s = 255, .v = 255 }, .index = 254 },
-	.layer_3_indicator = { .color = { .h = 84, .s = 255, .v = 0 }, .index = 71 },
+	.layer_3_indicator = { .color = { .h = 84, .s = 255, .v = 255 }, .index = 255 },
 	.alphas_mods = {
 		BACKLIGHT_ALPHAS_MODS_ROW_0,
 		BACKLIGHT_ALPHAS_MODS_ROW_1,
