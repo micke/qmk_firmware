6523e7a5e11af103f018f9d24159cf56f956a087
Micke Lisinge <me@mike.gg>
2018-06-09 23:03:14 +0200

    Remove unused tapdance

--- keyboards/zeal60/keymaps/mike/keymap.c
+++ keyboards/zeal60/keymaps/mike/keymap.c
@@ -14,37 +14,21 @@
 
 #define KC_CLEP M(M_CLEP)
 
-#define KC_SPC_OR_FUN LT(_FUN,  KC_SPC)
-
-enum {
-  TD_FUN2_OR_SWITCH_DEFAULT  = 0
-};
-
-#define KC_FUN2_OR_SWITCH_DEFAULT TD(TD_FUN2_OR_SWITCH_DEFAULT)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_DEFAULT] = {
-  {KC_ESC,   KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,           KC_8,    KC_9,     KC_0,     KC_MINS,  KC_EQL,    KC_BSLS},
-  {KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,           KC_I,    KC_O,     KC_P,     KC_LBRC,  KC_RBRC,   KC_BSPC},
-  {KC_LCTL,  KC_A,     KC_S,     KC_D,    KC_F,    KC_G,    KC_H,    KC_J,           KC_K,    KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,    KC_GRV },
-  {KC_LSFT,  KC_NO,    KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,           KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,   KC_FUN2_OR_SWITCH_DEFAULT},
-  {XXXXXX,   KC_LGUI,  KC_LALT,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  KC_SPC_OR_FUN,  XXXXXX,  XXXXXX,   XXXXXX,   KC_RALT,  KC_RGUI,   XXXXXX}
-},
-
-[_GAMING] = {
-  {KC_ESC,   KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,     KC_8,    KC_9,     KC_0,     KC_MINS,  KC_EQL,    KC_BSLS},
-  {KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,     KC_I,    KC_O,     KC_P,     KC_LBRC,  KC_RBRC,   KC_BSPC},
-  {KC_LCTL,  KC_A,     KC_S,     KC_D,    KC_F,    KC_G,    KC_LEFT,  KC_DOWN,  KC_UP,   KC_RGHT,  KC_SCLN,  KC_QUOT,  KC_ENT,    KC_GRV },
-  {KC_LSFT,  XXXXXX,   KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,     KC_N,     KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,   KC_FUN2_OR_SWITCH_DEFAULT},
-  {XXXXXX,   KC_LGUI,  KC_LALT,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,   KC_SPC,   XXXXXX,  XXXXXX,   XXXXXX,   KC_RALT,  KC_RGUI,   XXXXXX}
+  {KC_ESC,   KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSLS},
+  {KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSPC},
+  {KC_LCTL,  KC_A,     KC_S,     KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,   KC_GRV },
+  {KC_LSFT,  KC_NO,    KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  MO(_FUN)},
+  {XXXXXX,   KC_LGUI,  KC_LALT,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  KC_SPC,  XXXXXX,  XXXXXX,   XXXXXX,   KC_RALT,  KC_RGUI,  XXXXXX}
 },
 
 [_FUN] = {
-  {KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,   KC_F5,   KC_F6,    KC_F7,    KC_F8,   KC_F9,    KC_F10,   KC_F11,  KC_F12,  ______},
-  {______,   KC_HOME,  KC_PGUP,  ______,   ______,  ______,  ______,   ______,   ______,  ______,   KC_PSCR,  ______,  ______,  KC_DEL},
-  {______,   KC_END,   KC_PGDN,  KC_RGHT,  ______,  ______,  KC_LEFT,  KC_DOWN,  KC_UP,   KC_RGHT,  ______,   ______,  ______,  ______},
-  {______,   ______,   ______,   ______,   ______,  ______,  ______,   ______,   ______,  ______,   ______,   ______,  ______,  ______},
-  {______,   ______,   ______,   ______,   ______,  ______,  ______,   ______,   ______,  ______,   ______,   ______,  ______,  ______}
+  {KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,    ______},
+  {______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  KC_PSCR,  KC_UP,    ______,    ______},
+  {______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  KC_LEFT,  KC_RGHT,  ______,    ______},
+  {______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,   KC_DOWN,  ______,    ______},
+  {______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,   ______,   MO(_SYS),  ______}
 },
 
 [_FUN2] = {
@@ -87,28 +71,4 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
     }
   }
   return MACRO_NONE;
-};
-
-void dance_switch_finished (qk_tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    layer_on(_FUN2);
-  } else {
-    uint8_t deflayer = biton32(default_layer_state);
-
-    if (deflayer == _DEFAULT) {
-      default_layer_set(1UL<<_GAMING);
-    } else {
-      default_layer_set(1UL<<_DEFAULT);
-    }
-  }
-}
-
-void dance_switch_reset (qk_tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    layer_off(_FUN2);
-  }
-}
-
-qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_FUN2_OR_SWITCH_DEFAULT]  = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_switch_finished, dance_switch_reset)
 };
