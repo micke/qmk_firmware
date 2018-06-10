aca804f167f9db65f6d7a3c490696aa3695c4db7
Micke Lisinge <me@mike.gg>
2018-06-10 16:02:54 +0200

    Add swedish layer

--- keyboards/zeal60/keymaps/mike/keymap.c
+++ keyboards/zeal60/keymaps/mike/keymap.c
@@ -5,7 +5,7 @@
 #define XXXXXX KC_NO
 
 #define _DEFAULT 0
-#define _GAMING 1
+#define _SWEDISH 1
 #define _FUN 2
 #define _FUN2 3
 #define _SYS  4
@@ -42,6 +42,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_LCTL,  KC_A,     KC_S,     KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,   KC_GRV},
   {KC_LSFT,  KC_NO,    KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  MO(_FUN)},
   {XXXXXX,   KC_LGUI,  KC_LALT,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  KC_SPC,  XXXXXX,  XXXXXX,   XXXXXX,   KC_RALT,  KC_RGUI,  XXXXXX}
+},
+
+[_SWEDISH] = {
+  {______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,    ______,    ______,    ______},
+  {______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,    ______,    UC(0xE5),  ______},
+  {______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  UC(0xF6),  UC(0xE4),  ______,    ______},
+  {______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,    ______,    ______,    ______},
+  {______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,    ______,    ______,    ______}
 },
 
 [_FUN] = {
