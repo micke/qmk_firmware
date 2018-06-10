d71bfdc8fea8af1301b37a3dd1de20a8a9fe26e0
Micke Lisinge <me@mike.gg>
2018-06-10 13:55:49 +0200

    Hijack QK_MOMENTARY ... QK_MOMENTARY_MAX keycodes to switch layer without unregistering keys

--- keyboards/zeal60/keymaps/mike/keymap.c
+++ keyboards/zeal60/keymaps/mike/keymap.c
@@ -14,11 +14,29 @@ enum custom_keycodes {
   KC_CLEP = SAFE_RANGE
 };
 
+void non_clearing_layer_state_set(uint32_t state)
+{
+    dprint("layer_state: ");
+    layer_debug(); dprint(" to ");
+    layer_state = state;
+    layer_debug(); dprintln();
+}
+
+void non_clearing_layer_on(uint8_t layer)
+{
+    non_clearing_layer_state_set(layer_state | (1UL<<layer));
+}
+
+void non_clearing_layer_off(uint8_t layer)
+{
+    non_clearing_layer_state_set(layer_state & ~(1UL<<layer));
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_DEFAULT] = {
   {KC_ESC,   KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSLS},
   {KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSPC},
-  {KC_LCTL,  KC_A,     KC_S,     KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,   KC_GRV },
+  {KC_LCTL,  KC_A,     KC_S,     KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,   KC_GRV},
   {KC_LSFT,  KC_NO,    KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  MO(_FUN)},
   {XXXXXX,   KC_LGUI,  KC_LALT,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  KC_SPC,  XXXXXX,  XXXXXX,   XXXXXX,   KC_RALT,  KC_RGUI,  XXXXXX}
 },
@@ -27,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,    ______},
   {______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  KC_PSCR,  KC_UP,    ______,    ______},
   {______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  KC_LEFT,  KC_RGHT,  ______,    ______},
-  {______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,   KC_DOWN,  ______,    ______},
+  {______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,   KC_DOWN,  MO(_FUN2),    ______},
   {______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,   ______,   MO(_SYS),  ______}
 },
 
@@ -36,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {______,  ______,  ______,  ______,  ______,  ______,  ______,   ______,   ______,   ______,   ______,   ______,  ______,    ______},
   {______,  ______,  ______,  ______,  ______,  ______,  KC_MRWD,  KC_VOLD,  KC_VOLU,  KC_MFFD,  KC_MPLY,  ______,  ______,    ______},
   {______,  ______,  ______,  ______,  ______,  ______,  ______,   ______,   ______,   ______,   ______,   ______,  ______,    ______},
-  {______,  ______,  ______,  ______,  ______,  ______,  ______,   ______,   ______,   ______,   ______,   ______,  MO(_SYS),  ______}
+  {______,  ______,  ______,  ______,  ______,  ______,  ______,   ______,   ______,   ______,   ______,   ______,  ______,  ______}
 },
 
 [_SYS] = {
@@ -50,17 +68,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-    switch(keycode) {
-      case KC_CLEP:
-        // Set the Zeal60 specific EEPROM state as invalid.
-        eeprom_set_valid(false);
-        // Set the TMK/QMK EEPROM state as invalid.
-        eeconfig_disable();
-        // Jump to bootloader.
-        bootloader_jump();
-        return false;
-    }
+  uint8_t action_layer;
+
+  switch(keycode) {
+    case KC_CLEP:
+      // Set the Zeal60 specific EEPROM state as invalid.
+      eeprom_set_valid(false);
+      // Set the TMK/QMK EEPROM state as invalid.
+      eeconfig_disable();
+      // Jump to bootloader.
+      bootloader_jump();
+      return false;
+    case QK_MOMENTARY ... QK_MOMENTARY_MAX:
+      action_layer = keycode & 0xFF;
+
+      if (record->event.pressed) {
+        non_clearing_layer_on(action_layer);
+      } else {
+        non_clearing_layer_off(action_layer);
+      }
+
+      return false;
   }
+
   return true;
 };
