2678b6d33a53b4e87f249c2bd9f063178001fc3c
Micke Lisinge <me@mike.gg>
2018-07-01 14:54:47 +0200

    Fix LGUI

--- keyboards/zeal60/keymaps/mike/keymap.c
+++ keyboards/zeal60/keymaps/mike/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,      KC_I,    KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSPC},
   {KC_LCTL,  KC_A,     KC_S,     KC_D,    KC_F,    KC_G,    KC_H,    KC_J,      KC_K,    KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,   KC_GRV},
   {KC_LSPO,  KC_NO,    KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,      KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSPC,  KC_FUN_OR_SWITCH_DEFAULT},
-  {XXXXXX,   KC_LGUI,  KC_LALT,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  KC_SPCOF,  XXXXXX,  XXXXXX,   XXXXXX,   KC_RALT,  XXXXXX,   XXXXXX}
+  {XXXXXX,   KC_LGUI,  KC_LALT,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  KC_SPCOF,  XXXXXX,  XXXXXX,   XXXXXX,   KC_RALT,  KC_RGUI,   XXXXXX}
 },
 
 [_GAMING] = {
