6ed84cdc7341501d718d40571c5a7cf517899a33
Micke Lisinge <hi@micke.me>
2020-07-22 20:57:07 +0200

    Add mickes physix keymap

---
+++ keyboards/hineybush/physix/keymaps/micke/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+#define IGNORE_MOD_TAP_INTERRUPT
+#define PERMISSIVE_HOLD
+/* #define TAPPING_TERM 150 */
+
+#define FORCE_NKRO
+
+#endif
